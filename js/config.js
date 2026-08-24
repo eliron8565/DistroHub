4be6b6c8e478b6064da21cd8bf419309e3ad05ae
Eliron <eliron855@gmail.com>
2026-08-24 17:34:16 +0300

    chore: add browser-safe Supabase configuration

---
+++ js/config.js
@@ -0,0 +1,5 @@
+/*
+ * Browser-safe DistroHub configuration.
+ * Paste your Supabase Publishable Key here. Never put a service_role/secret key in this file.
+ */
+window.DISTROHUB_SUPABASE_PUBLISHABLE_KEY = '';
